8a7a1da2980363b500f4a06d7c015ee3d4e545bd
Kade Illian <kadeillianmt@gmail.com>
2022-09-01 13:05:03 -0600

    added CRUD functionality to the app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,144 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Song.create(
+  title: "Mr. Blue Sky",
+  album: "Out of the Blue",
+  artist: "Electric Lights Orchestra",
+  year: 1977,
+  lyrics: "Sun is shinin' in the sky
+  There ain't a cloud in sight
+  It's stopped rainin' everybody's in the play
+  And don't you know
+  It's a beautiful new day, hey hey
+  Runnin' down the avenue
+  See how the sun shines brightly in the city
+  On the streets where once was pity
+  Mr. Blue Sky is living here today, hey hey
+  Mr. Blue Sky please tell us why
+  You had to hide away for so long (so long)
+  Where did we go wrong?
+  Mr. Blue Sky please tell us why
+  You had to hide away for so long (so long)
+  Where did we go wrong?
+  Hey you with the pretty face
+  Welcome to the human race
+  A celebration, Mr. Blue Sky's up there waitin'
+  And today is the day we've waited for
+  Oh Mr. Blue Sky please tell us why
+  You had to hide away for so long (so long)
+  Where did we go wrong?
+  Hey there Mr. Blue
+  We're so pleased to be with you
+  Look around see what you do
+  Everybody smiles at you
+  Hey there Mr. Blue
+  We're so pleased to be with you
+  Look around see what you do
+  Everybody smiles at you
+  Mr. Blue, you did it right
+  But soon comes Mr. Night creepin' over
+  Now his hand is on your shoulder
+  Never mind I'll remember you this
+  I'll remember you this way
+  Mr. Blue Sky please tell us why
+  You had to hide away for so long (so long)
+  Where did we go wrong?
+  Hey there Mr. Blue (sky)
+  We're so pleased to be with you (sky)
+  Look around see what you do (blue)
+  Everybody smiles at you",
+)
+
+Song.create(
+  title: "Don't Stop Believin'",
+  album: "Escape",
+  artist: "Journey",
+  year: 1981,
+  lyrics: "Just a small town girl
+  Livin' in a lonely world
+  She took the midnight train going anywhere
+  Just a city boy
+  Born and raised in South Detroit
+  He took the midnight train going anywhere
+  A singer in a smokey room
+  A smell of wine and cheap perfume
+  For a smile they can share the night
+  It goes on and on and on and on
+  Strangers waitin'
+  Up and down the boulevard
+  Their shadows searchin' in the night
+  Streetlights, people
+  Livin' just to find emotion
+  Hidin', somewhere in the night
+  Workin' hard to get my fill
+  Everybody wants a thrill
+  Payin' anything to roll the dice
+  Just one more time
+  Some'll win, some will lose
+  Some are born to sing the blues
+  Whoa, the movie never ends
+  It goes on and on and on and on
+  Strangers waitin'
+  Up and down the boulevard
+  Their shadows searchin' in the night
+  Streetlights, people
+  Livin' just to find emotion
+  Hidin', somewhere in the night
+  Don't stop believin'
+  Hold on to that feelin'
+  Streetlights, people
+  Don't stop believin'
+  Hold on
+  Streetlights, people
+  Don't stop believin'
+  Hold on to that feelin'
+  Streetlights, people",
+)
+
+Song.create(
+  title: "Telephone Line",
+  album: "A New World Record",
+  artist: "Electric Lights Orchestra",
+  year: 1976,
+  lyrics: "Hello, how are you?
+  Have you been alright
+  Through all those lonely, lonely, lonely, lonely, lonely nights?
+  That's what I'd say, I'd tell you everything
+  If you'd pick up that telephone
+  Yeah, yeah, yeah
+  Hey, how're you feelin'?
+  Are you still the same?
+  Don't you realise the things we did, we did, were all for real?
+  Not a dream
+  I just can't believe they've all faded out of view
+  Yeah, yeah, yeah, yeah
+  Ooh-ooh-ooh
+  I look into the sky
+  (The love you need ain't gonna see you through)
+  And I wonder why
+  (The little things you planned ain't coming true)
+  Oh, oh, telephone line, give me some time
+  I'm living in twilight
+  Oh, oh, telephone line, give me some time
+  I'm living in twilight
+  Okay, so no one's answering
+  Well, can't you just let it ring a little longer, longer, longer
+  Oh, I'll just sit tight
+  In shadows of the night
+  Let it ring forevermore, oh-woh
+  Yeah, yeah, yeah
+  I look into the sky
+  (The love you need ain't gonna see you through)
+  And I wonder why
+  (The little things you planned ain't coming true)
+  Oh, oh, telephone line, give me some time
+  I'm living in twilight
+  Oh, oh, telephone line, give me some time
+  I'm living in twilight
+  Oh, oh, telephone line, give me some time
+  I'm living in twilight
+  Oh, oh, telephone line, give me some time
+  I'm living in twilight",
+)
